a2e139efbd71414dc76a08214e003f072e178776
gouthamstantri <51639089+gouthamstantri@users.noreply.github.com>
2024-12-12 21:14:28 +0530

    Add files via upload

---
+++ flop.v
@@ -0,0 +1,10 @@
+module flopr(
+  input clk, reset,
+  input [31:0] d,
+  output reg [31:0] q);
+	always @ (posedge clk, posedge reset)
+      if (reset) q<=0;
+	  else q<=d;
+endmodule
+
+//for PC flop is used with clock ason instruction cycle
